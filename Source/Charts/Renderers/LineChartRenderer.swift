09370c756e01bd7dd8dc0c742f6fd41a06e5905b
Chad_Hsu <Chad_hsu@cmoney.com.tw>
2022-06-14 17:50:48 +0800

    fix(mod): 試解charts閃退問題

--- Source/Charts/Renderers/LineChartRenderer.swift
+++ Source/Charts/Renderers/LineChartRenderer.swift
@@ -828,7 +828,8 @@ extension LineChartRenderer {
             _lineSegments = [CGPoint](repeating: CGPoint(), count: pointsPerEntryPair)
         }
 
-        for j in _xBounds.dropLast() {
+        let xBounds = _xBounds.dropLast()
+        for j in xBounds {
             var e: ChartDataEntry! = dataSet.entryForIndex(j)
             
             if e == nil { continue }
@@ -838,9 +839,6 @@ extension LineChartRenderer {
             let valueStart = _lineSegments[0]
             let valueEnd: CGPoint
             if j < _xBounds.max {
-                // TODO: remove the check.
-                // With the new XBounds iterator, j is always smaller than _xBounds.max
-                // Keeping this check for a while, if xBounds have no further breaking changes, it should be safe to remove the check
                 e = dataSet.entryForIndex(j + 1)
                 if e == nil { break }
                 valueEnd = CGPoint(x: CGFloat(e.x), y: CGFloat(e.y * phaseY))
